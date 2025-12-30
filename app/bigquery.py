1dc9a68894d99da8144065ad06acb1e16ad724f8
R-CCI <ymejia@cci.com.do>
2025-12-30 13:09:00 -0400

    Update and rename bq.py to bigquery.py

---
+++ app/bigquery.py
@@ -0,0 +1,11 @@
+def get_active_subscribers() -> list[str]:
+    """
+    Placeholder para BigQuery.
+    """
+    return []
+
+def unsubscribe_email(email: str):
+    """
+    Placeholder unsubscribe.
+    """
+    return True
